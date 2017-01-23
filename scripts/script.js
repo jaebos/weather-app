4c61f543838e04c541770a2789cfd3e62ba94234
jaebos <millerjt82@gmail.com>
2017-01-23 06:55:36 -0500

    code changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,27 @@
+$(document).ready(function(){
+  var long;
+  var lat;
+  var temp;
 
+      if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+
+//Create API with geolocation
+  var api = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=a6883ff8993a6da2e03aa69d6464ba6b';
+
+  $.getJSON(api, function(data){
+
+    var city = data.name;
+
+    console.log(city);
+    console.log(api);
+
+  });
+});
+}
+
+});
